efa4c8e05fd012ff063da75e2382788aca61ec0e
Luigi <russo.1699981@studenti.uniroma1.it>
2019-08-04 20:57:53 +0200

    setup.py for future packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pywmit",
+    version="0.1.0",
+    author="Luigi Russo",
+    author_email="russo.1699981@studenti.uniroma1.it",
+    description="A simple package to interact Wikipedia pages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lrusso96/pywmit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['mwparserfromhell', 'pywikibot']
+)
