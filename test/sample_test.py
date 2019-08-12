575ed1850a7ce9230e60e26fd3b343a27d0f85ab
Luigi <russo.1699981@studenti.uniroma1.it>
2019-08-12 12:40:00 +0200

    update test

--- test/sample_test.py
+++ test/sample_test.py
@@ -1,27 +1,11 @@
-"""from pywmit.templates.cite import CitaWeb, CitaPubblicazione, CitaLibro
-from pywmit.parser import parse
+from pywmit.check import check
 import unittest
 
 
 class TestSampleMethods(unittest.TestCase):
     def test_simple(self):
-        wikicode = parse("Placca iberica")
-        templates = wikicode.filter_templates()
-        cita_web = CitaWeb()
-        self.assertEqual(cita_web.name, 'cita web')
-        cita_pubblicazione = CitaPubblicazione()
-        cita_libro = CitaLibro()
-        for t in templates:
-            a = t.params
-            if t.name.matches('cita web'):
-                cita_web.check_params(a)
-            elif t.name.matches('cita pubblicazione'):
-                cita_pubblicazione.check_params(a)
-            elif t.name.matches('cita libro'):
-                cita_libro.check_params(a)
-        print(wikicode)
+        check("Council of Fashion Designers of America")
 
 
 if __name__ == '__main__':
     unittest.main()
-"""
